b8c8985b51f836c830a69fb0132444a6ea4d16d3
slendyG <josuegarza0608@gmail.com>
2022-10-19 21:57:38 -0700

    Added home and user routes

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars')
 const routes = require('./Controllers');
-const helpers = require('');
+// const helpers = require('');
 // sequelize connection
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
